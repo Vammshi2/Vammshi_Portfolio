94fb5d6c85af68472099af630e7fbd326ebc4330
Vamshikrishna <162019269+Vammshi2@users.noreply.github.com>
2025-06-18 18:04:16 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           className="hero-resume"
           onClick={() =>
             window.open(
-              'https://drive.google.com/file/d/18F6VIgjlP2oac8Ay4UbPUhDwITRm5GWa/view?usp=sharing',
+              'https://drive.google.com/file/d/1MdVBapcEGrB55jkLNUQPISG22H26SRWN/view?usp=drive_link',
               '_blank'
             )
           }
